acea32cb07dc719ced20d8660ff7fe4f219a6aa9
s z <13377528276@163.com>
2026-04-23 15:58:59 +0800

    fix

---
+++ capacitor.config.js
@@ -0,0 +1,13 @@
+const config = {
+  appId: 'com.travelmemory.app',
+  appName: '小旅行',
+  webDir: '.',
+  server: {
+    androidScheme: 'https'
+  },
+  android: {
+    allowMixedContent: true
+  }
+};
+
+module.exports = config;
